bad1cfe03455a166941028b11734091608c612ce
hanzheng <hanzheng@debian.(none)>
2013-05-20 18:06:48 +0800

    格式化

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 chsh -s /bin/bash
-mv ~/.vim ~/.vim.bak
-ln -s .rc/vim ~/.vim
 who=$(whoami)
 cd
-for n in ctags bashrc bash_profile bash_aliases vimrc inputrc screenrc lftp gitrc gitconfig.${who}
+for n in vim ctags bashrc bash_profile bash_aliases vimrc inputrc screenrc lftp gitrc gitconfig.${who}
 do
 	ln -sfv -T .rc/$n .$(basename $n .$who)
 done
