56c4bdb4fb19c8da41c695a06d0942021c834962
hanzheng <hzkoala@gmail.com>
2014-08-07 02:27:22 +0000

    Fix:统一gitconfig配置

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 chsh -s /bin/bash
 who=$(whoami)
 cd
-for n in vim man bashrc bash_profile bash_aliases vimrc inputrc screenrc gitrc gitconfig.${who} git-completion.bash
+for n in vim man bashrc bash_profile bash_aliases vimrc inputrc screenrc gitrc gitconfig git-completion.bash
 do
 	ln -sfv -T .rc/$n .$(basename $n .$who)
 done
